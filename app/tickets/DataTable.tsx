e7de4d49e88c16c980828b619efd2e98601e3799
Nicolas Rodriguez <alvaro.delgado@quandoo.com>
2024-04-24 14:07:45 +0200

    status badge

--- app/tickets/DataTable.tsx
+++ app/tickets/DataTable.tsx
@@ -1,3 +1,12 @@
+import TicketStatusBadge from "@/components/TicketStatusBadge";
+import {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  Table,
+} from "@/components/ui/table";
 import { Ticket } from "@prisma/client";
 import React from "react";
 
@@ -7,15 +16,44 @@ interface Props {
 
 const DataTable = ({ tickets }: Props) => {
   return (
-    <div>
-      {tickets.map((ticket) => (
-        <div key={ticket.id}>
-          <h3>{ticket.title}</h3>
-          <p>Description: {ticket.description}</p>
-          <p>Status: {ticket.status}</p>
-          <p>Priority: {ticket.priority}</p>
-        </div>
-      ))}
+    <div className="w-full mt-5">
+      <div className="rounded-md sm:boder">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Title</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Priority</TableHead>
+              <TableHead>CreatedAt</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tickets
+              ? tickets.map((ticket) => (
+                  <TableRow key={ticket.id} data-ref="/">
+                    <TableCell>{ticket.title}</TableCell>
+                    <TableCell>
+                      <div className="flex justify-center">
+                        <TicketStatusBadge status={ticket.status} />
+                      </div>
+                    </TableCell>
+                    <TableCell>{ticket.priority}</TableCell>
+                    <TableCell>
+                      {ticket.createdAt.toLocaleDateString("en-US", {
+                        year: "2-digit",
+                        month: "2-digit",
+                        day: "2-digit",
+                        hour: "numeric",
+                        minute: "2-digit",
+                        hour12: true,
+                      })}
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 };
