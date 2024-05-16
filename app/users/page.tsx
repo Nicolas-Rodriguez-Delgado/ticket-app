74c064cd68df5922117a6446dd7ca680c90f8496
Nicolas-Rodriguez-Delgado <spastikcitizen@gmail.com>
2024-05-16 19:48:52 +0200

    enable creating user

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,10 +5,10 @@ import { getServerSession } from "next-auth";
 import options from "../api/auth/[...nextauth]/options";
 
 export default async function Users() {
-  const session = await getServerSession(options);
-  if (session?.user.role !== "ADMIN") {
-    return <p className="text-destructive">Admin access required!</p>;
-  }
+  // const session = await getServerSession(options);
+  // if (session?.user.role !== "ADMIN") {
+  //   return <p className="text-destructive">Admin access required!</p>;
+  // }
 
   const users = await prisma.user.findMany();
 
